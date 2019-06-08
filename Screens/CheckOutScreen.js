707bef9da1f1e1d7d5feebd1b16c0ecf4a775feb
Max <38254068+nmax93@users.noreply.github.com>
2019-06-08 22:18:39 +0300

    Update CheckOutScreen.js

--- Screens/CheckOutScreen.js
+++ Screens/CheckOutScreen.js
@@ -16,12 +16,19 @@ const styles = StyleSheet.create({
 export default class CheckOutScreen extends Component {
   constructor(props) {
     super(props)
+    this.state = { total: '' }
     this.mapOrders = this.mapOrders.bind(this)
     this.finishProcess = this.finishProcess.bind(this)
+    this.updateTotal = this.updateTotal.bind(this)
+    this.generateTotal = this.generateTotal.bind(this)
   }
 
   static propTypes = {
     navigation: PropTypes.object.isRequired
+  }
+
+  componentDidMount() {
+    this.generateTotal()
   }
 
   mapOrders() {
@@ -34,25 +41,45 @@ export default class CheckOutScreen extends Component {
     return orders
   }
 
+  updateTotal(newTotal) {
+    this.setState({ total: newTotal })
+  }
+
+  generateTotal() {
+    const { navigation } = this.props
+    const cart = navigation.getParam('cart')
+    let price = 0
+    cart.forEach(order => {
+      price += order.product.price
+      order.addOns.forEach(addOn => (price += addOn.price))
+    })
+    const rounded = Math.round(price)
+    if (price - rounded === 0) this.setState({ total: `${price}.00` })
+    else this.setState({ total: `${price}0` })
+  }
+
   finishProcess() {
     const { navigation } = this.props
     const emptyCart = navigation.getParam('emptyCart')
     const MainScreenDishes = navigation.getParam('dishes')
     const addToCart = navigation.getParam('addToCart')
-    emptyCart()
-    navigation.navigate('ConfirmationScreen', { dishes: MainScreenDishes, addToCart: addToCart })
+    const cart = navigation.getParam('cart')
+    navigation.navigate('ConfirmationScreen', {
+      dishes: MainScreenDishes,
+      addToCart: addToCart,
+      emptyCart: emptyCart,
+      total: this.state.total,
+      cart: cart
+    })
   }
 
   render() {
-    const { navigation } = this.props
-    const cart = navigation.getParam('cart')
-
     return (
       <View style={styles.container}>
         <ScrollView>
           <TitleBar title="Your orders" />
           {this.mapOrders()}
-          <CheckOutTotal ordersList={cart} />
+          <CheckOutTotal total={this.state.total} />
           <PrettyButton text="CHECK OUT" handlePress={this.finishProcess} />
         </ScrollView>
       </View>
