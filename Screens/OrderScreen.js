00eaa784e0ca4698f8251a7d8b0a6730c7c65c9f
Max <38254068+nmax93@users.noreply.github.com>
2019-06-11 18:54:00 +0300

    Update OrderScreen.js

--- Screens/OrderScreen.js
+++ Screens/OrderScreen.js
@@ -4,6 +4,7 @@ import OrderDetails from '../Components/OrderScreenComponents/OrderDetails'
 import OrderImage from '../Components/OrderScreenComponents/OrderImage'
 import AddOnsButton from '../Components/OrderScreenComponents/AddOnsButton'
 import AddOnsList from '../Components/OrderScreenComponents/AddOnsList'
+import FeedbackBox from '../Components/OrderScreenComponents/FeedbackBox'
 import SeparationLine from '../Components/SeparationLine'
 import PrettyButton from '../Components/PrettyButton'
 import PropTypes from 'prop-types'
@@ -20,7 +21,9 @@ export default class OrderScreen extends Component {
     super(props)
     this.state = { selectedAddOns: [] }
     this.AddOnsListController = React.createRef()
+    this.FeedbackBoxController = React.createRef()
     this.getUserAddOn = this.getUserAddOn.bind(this)
+    this.handleOrder = this.handleOrder.bind(this)
   }
 
   static propTypes = {
@@ -42,10 +45,18 @@ export default class OrderScreen extends Component {
     }
   }
 
+  handleOrder() {
+    const { navigation } = this.props
+    const addToCart = navigation.getParam('addToCart')
+    const product = navigation.getParam('product')
+
+    addToCart(product, this.state.selectedAddOns)
+    this.FeedbackBoxController.current.feedBackFadeIn()
+  }
+
   render() {
     const { navigation } = this.props
     const product = navigation.getParam('product')
-    const addToCart = navigation.getParam('addToCart')
 
     return (
       <View style={styles.container}>
@@ -59,12 +70,10 @@ export default class OrderScreen extends Component {
           <SeparationLine />
           <AddOnsButton controller={this.AddOnsListController} />
           <SeparationLine />
-          <PrettyButton
-            text="ADD TO CART"
-            handlePress={() => addToCart(product, this.state.selectedAddOns)}
-          />
+          <PrettyButton text="ADD TO CART" handlePress={this.handleOrder} />
           <SeparationLine />
         </ScrollView>
+        <FeedbackBox ref={this.FeedbackBoxController} />
         <AddOnsList
           ref={this.AddOnsListController}
           addOns={product.addOns}
